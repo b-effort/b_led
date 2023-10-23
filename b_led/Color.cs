07d8cd1f0bfd9a41a38eda812cec672e55cae739
Ryan Shea <ryan@b-effort.games>
2023-10-22 23:08:26 -0500

    improve preview rendering

---
+++ b_led/Color.cs
@@ -0,0 +1,103 @@
+using System.Runtime.CompilerServices;
+
+static class Color {
+	public readonly record struct RGB(byte r, byte g, byte b) {
+		public static implicit operator rlColor(RGB @this) => new(@this.r, @this.g, @this.b, (byte)255);
+	}
+
+	public readonly record struct HS(float h, float s);
+
+	public readonly record struct HSB(float h, float s, float b) {
+		public HSB(HS hs, float b) : this(hs.h, hs.s, b) { }
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public RGB ToRGB() {
+			if (this.s == 0) {
+				var value = (byte)(this.b * 255 + 0.5f);
+				return new RGB(value, value, value);
+			}
+
+			float chroma = this.b * this.s;
+			float hue60 = this.h * 6f;
+			float x = chroma * (1 - Abs(hue60 % 2 - 1));
+			float r, g, b;
+
+			switch (hue60) {
+				case >= 0 and < 1:
+					r = chroma;
+					g = x;
+					b = 0;
+					break;
+				case >= 1 and < 2:
+					r = x;
+					g = chroma;
+					b = 0;
+					break;
+				case >= 2 and < 3:
+					r = 0;
+					g = chroma;
+					b = x;
+					break;
+				case >= 3 and < 4:
+					r = 0;
+					g = x;
+					b = chroma;
+					break;
+				case >= 4 and < 5:
+					r = x;
+					g = 0;
+					b = chroma;
+					break;
+				default:
+					r = chroma;
+					g = 0;
+					b = x;
+					break;
+			}
+
+			double m = this.b - chroma;
+
+			return new RGB(
+				r: (byte)((r + m) * 255 + 0.5f),
+				g: (byte)((g + m) * 255 + 0.5f),
+				b: (byte)((b + m) * 255 + 0.5f)
+			);
+		}
+	}
+
+	public readonly record struct HSL(float h, float s, float l) {
+		public HSL(HS hs, float l) : this(hs.h, hs.s, l) { }
+
+		public static HSL FromRGB(RGB rgb) => FromRGB(rgb.r / 255f, rgb.g / 255f, rgb.b / 255f);
+
+		public static HSL FromRGB(float r, float g, float b) {
+			float max = Max(r, Max(g, b));
+			float min = Min(r, Min(g, b));
+			float h, s, l;
+
+			l = (max + min) / 2f;
+
+			if (max == min) {
+				h = s = 0;
+			} else {
+				float diff = max - min;
+
+				s = l > 0.5f
+					? diff / (2f - max - min)
+					: diff / (max + min);
+
+				if (max == r) {
+					h = (g - b) / diff + (g < b ? 6 : 0);
+				} else if (max == g) {
+					h = (b - r) / diff + 2;
+				} else {
+					h = (r - g) / diff + 4;
+				}
+
+				h /= 6;
+			}
+
+			return new HSL(h, s, l);
+		}
+	}
+}
--- b_led/Program.cs
+++ b_led/Program.cs
@@ -6,12 +6,11 @@ global using rlImGui_cs;
 global using ImGuiNET;
 //# aliases
 global using rl = Raylib_cs.Raylib;
-//# static
-global using static System.MathF;
+global using rlColor = Raylib_cs.Color;
 using System.Diagnostics;
-using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
 using Raylib_cs;
-using rlColor = Raylib_cs.Color;
+//# static
 
 /*
 # Mapping
@@ -42,8 +41,8 @@ unsafe {
 }
 
 var state = new State() {
-	Generator = new SineWaveGenerator(),
-	ColorGenerator = new RainbowColorGenerator(),
+	Generator = new SinGenerator(),
+	ColorGenerator = new HSBDemoColorGenerator(),
 };
 
 using var previewWindow = new PreviewWindow(state);
@@ -83,32 +82,25 @@ void DrawUI() {
 }
 
 sealed class State {
-	public const int Resolution = 256;
+	public const int BufferSize = 128;
 
 	public required Generator Generator { get; set; }
 	public required ColorGenerator ColorGenerator { get; set; }
 
-	public readonly float[,] brightnessBuffer = new float[Resolution, Resolution];
-	public readonly ColorHS[,] hsBuffer = new ColorHS[Resolution, Resolution];
-	public readonly ColorHSB[,] hsbBuffer = new ColorHSB[Resolution, Resolution];
-	public readonly ColorRGB[,] outputBuffer = new ColorRGB[Resolution, Resolution];
+	public readonly Color.RGB[,] outputBuffer = new Color.RGB[BufferSize, BufferSize];
 
 	public void Update() {
-		var brightnesses = this.brightnessBuffer;
-		var colorsHs = this.hsBuffer;
-		var colorsHsb = this.hsbBuffer;
 		var outputs = this.outputBuffer;
 
-		for (var x = 0; x < Resolution; x++) {
-			for (var y = 0; y < Resolution; y++) {
-				int i = x + y * Resolution;
-				float xPercent = x / (float)(Resolution - 1);
-				float yPercent = y / (float)(Resolution - 1);
+		for (var x = 0; x < BufferSize; x++) {
+			for (var y = 0; y < BufferSize; y++) {
+				int i = x + y * BufferSize;
+				float xPercent = x / (float)(BufferSize - 1);
+				float yPercent = y / (float)(BufferSize - 1);
 
-				var brightness = brightnesses[x, y] = this.Generator.Generate(i, xPercent, yPercent);
-				var hs = colorsHs[x, y] = this.ColorGenerator.Generate(i, xPercent, yPercent);
-				var hsb = colorsHsb[x, y] = new ColorHSB(hs, brightness);
-				outputs[x, y] = hsb.ToRGB();
+				var brightness = this.Generator.Generate(i, xPercent, yPercent);
+				var hs = this.ColorGenerator.Generate(i, xPercent, yPercent);
+				outputs[x, y] = new Color.HSB(hs, brightness).ToRGB();
 			}
 		}
 	}
@@ -117,11 +109,17 @@ sealed class State {
 #region windows
 
 sealed class PreviewWindow : IDisposable {
-	readonly RenderTexture2D rt = rl.LoadRenderTexture(State.Resolution, State.Resolution);
+	const int Resolution = State.BufferSize;
+
+	readonly Image image;
+	readonly Texture2D texture;
+
 	readonly State state;
 
 	public PreviewWindow(State state) {
 		this.state = state;
+		this.image = rl.GenImageColor(Resolution, Resolution, rlColor.BLACK);
+		this.texture = rl.LoadTextureFromImage(this.image);
 	}
 
 	~PreviewWindow() {
@@ -129,37 +127,36 @@ sealed class PreviewWindow : IDisposable {
 	}
 
 	public void Dispose() {
-		rl.UnloadRenderTexture(this.rt);
+		rl.UnloadImage(this.image);
+		rl.UnloadTexture(this.texture);
 		GC.SuppressFinalize(this);
 	}
 
-	public void Update() {
-		rl.BeginTextureMode(this.rt);
-		{
-			rl.ClearBackground(rlColor.BLACK);
-			var outputBuffer = this.state.outputBuffer;
-			var resolution = outputBuffer.GetLength(0);
-			for (var x = 0; x < resolution; x++) {
-				for (var y = 0; y < resolution; y++) {
-					rl.DrawPixel(x, y, outputBuffer[x, y]);
-				}
+	public unsafe void Update() {
+		var pixels = (rlColor*)this.image.data;
+		var buffer = this.state.outputBuffer;
+		for (var x = 0; x < Resolution; x++) {
+			for (var y = 0; y < Resolution; y++) {
+				pixels[x + y * Resolution] = buffer[x, y];
 			}
 		}
-		rl.EndTextureMode();
+
+		rl.UpdateTexture(this.texture, pixels);
 	}
 
 	public void Show() {
 		ImGui.SetNextWindowSize(new Vector2(512));
 		ImGui.Begin("preview");
 		{
-			// Vector2 size = ImGui.GetContentRegionAvail();
-			rlImGui.ImageRenderTextureFit(this.rt);
+			imUtil.ImageTextureFit(this.texture);
 		}
 		ImGui.End();
 	}
 }
 
 #endregion
+
+#region framework stuff
 
 interface Generator {
 	float Generate(int index, float x, float y);
@@ -176,7 +173,7 @@ class GeneratorSlot {
 }
 
 interface ColorGenerator {
-	ColorHS Generate(int index, float x, float y);
+	Color.HS Generate(int index, float x, float y);
 }
 
 class GeneratorGroup : Generator {
@@ -217,9 +214,9 @@ static class IntensityBlendModeExtensions {
 		return mode switch {
 			IntensityBlendMode.LAST => b,
 			IntensityBlendMode.AND  => b >= threshold ? a : 0,
-			IntensityBlendMode.OR   => Max(a, b),
+			IntensityBlendMode.OR   => MathF.Max(a, b),
 			IntensityBlendMode.NOT  => b >= threshold ? 0 : a,
-			IntensityBlendMode.XOR  => (a >= threshold) ^ (b >= threshold) ? Max(a, b) : 0,
+			IntensityBlendMode.XOR  => (a >= threshold) ^ (b >= threshold) ? MathF.Max(a, b) : 0,
 			IntensityBlendMode.ADD  => a + b,
 			IntensityBlendMode.SUB  => a - b,
 			IntensityBlendMode.AVG  => (a + b) / 2,
@@ -228,117 +225,27 @@ static class IntensityBlendModeExtensions {
 	}
 }
 
-#region color
-
-record struct ColorRGB(int r, int g, int b) {
-	public static implicit operator rlColor(ColorRGB @this) => new(@this.r, @this.g, @this.b, 255);
-}
-
-record struct ColorHS(float h, float s) { }
-
-record struct ColorHSB(float h, float s, float b) {
-	public ColorHSB(ColorHS hs, float b) : this(hs.h, hs.s, b) { }
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public ColorRGB ToRGB() {
-		if (this.s == 0) {
-			var value = (int)(this.b * 255 + 0.5f);
-			return new ColorRGB(value, value, value);
-		}
-
-		float chroma = this.b * this.s;
-		float hue60 = this.h * 6f;
-		float x = chroma * (1 - Abs(hue60 % 2 - 1));
-		float r, g, b;
-
-		switch (hue60) {
-			case >= 0 and < 1:
-				r = chroma;
-				g = x;
-				b = 0;
-				break;
-			case >= 1 and < 2:
-				r = x;
-				g = chroma;
-				b = 0;
-				break;
-			case >= 2 and < 3:
-				r = 0;
-				g = chroma;
-				b = x;
-				break;
-			case >= 3 and < 4:
-				r = 0;
-				g = x;
-				b = chroma;
-				break;
-			case >= 4 and < 5:
-				r = x;
-				g = 0;
-				b = chroma;
-				break;
-			default:
-				r = chroma;
-				g = 0;
-				b = x;
-				break;
-		}
-
-		double m = this.b - chroma;
-
-		return new ColorRGB(
-			r: (int)((r + m) * 255 + 0.5f),
-			g: (int)((g + m) * 255 + 0.5f),
-			b: (int)((b + m) * 255 + 0.5f)
-		);
-	}
-}
-
-record struct ColorHSL(float h, float s, float l) {
-	public ColorHSL(ColorHS hs, float l) : this(hs.h, hs.s, l) { }
-
-	public static ColorHSL FromRGB(int r, int g, int b) => FromRGB(r / 255f, g / 255f, b / 255f);
-
-	public static ColorHSL FromRGB(float r, float g, float b) {
-		float max = Max(r, Max(g, b));
-		float min = Min(r, Min(g, b));
-		float h, s, l;
-
-		l = (max + min) / 2f;
-
-		if (max == min) {
-			h = s = 0;
-		} else {
-			float diff = max - min;
-
-			s = l > 0.5f
-				? diff / (2f - max - min)
-				: diff / (max + min);
-
-			if (max == r) {
-				h = (g - b) / diff + (g < b ? 6 : 0);
-			} else if (max == g) {
-				h = (b - r) / diff + 2;
-			} else {
-				h = (r - g) / diff + 4;
-			}
-
-			h /= 6;
-		}
-
-		return new ColorHSL(h, s, l);
-	}
-}
-
 #endregion
+
+static class Clock {
+	static readonly Stopwatch timer = Stopwatch.StartNew();
+
+	public static float T => (float)timer.Elapsed.TotalSeconds;
+}
 
 #region generators
 
-sealed class SineWaveGenerator : Generator {
+sealed class HSBDemoGenerator : Generator {
 	public float Generate(int index, float x, float y) {
 		return x >= 0.5 && y < 0.5
-			? (Sin((y - 0.25f) * 2 * PI) + 1) / 2
+			? (MathF.Sin((y - 0.25f) * 2 * MathF.PI) + 1) / 2
 			: 1;
+	}
+}
+
+sealed class SinGenerator : Generator {
+	public float Generate(int index, float x, float y) {
+		return MathF.Sin(x + Clock.T);
 	}
 }
 
@@ -346,9 +253,9 @@ sealed class SineWaveGenerator : Generator {
 
 #region color generators
 
-sealed class RainbowColorGenerator : ColorGenerator {
-	public ColorHS Generate(int index, float x, float y) {
-		return new ColorHS(
+sealed class HSBDemoColorGenerator : ColorGenerator {
+	public Color.HS Generate(int index, float x, float y) {
+		return new Color.HS(
 			x,
 			x < 0.5 && y < 0.5
 				? y * 2
@@ -358,3 +265,27 @@ sealed class RainbowColorGenerator : ColorGenerator {
 }
 
 #endregion
+
+static class imUtil {
+	[SuppressMessage("ReSharper", "PossibleLossOfFraction")]
+	public static void ImageTextureFit(Texture2D texture, bool center = true) {
+		Vector2 area = ImGui.GetContentRegionAvail();
+
+		float scale = area.X / texture.width;
+		float y = texture.height * scale;
+		if (y > area.Y) {
+			scale = area.Y / texture.height;
+		}
+
+		int sizeX = (int)(texture.width * scale);
+		int sizeY = (int)(texture.height * scale);
+
+		if (center) {
+			ImGui.SetCursorPosX(0);
+			ImGui.SetCursorPosX(area.X / 2 - sizeX / 2);
+			ImGui.SetCursorPosY(ImGui.GetCursorPosY() + (area.Y / 2 - sizeY / 2));
+		}
+
+		rlImGui.ImageSize(texture, sizeX, sizeY);
+	}
+}
